b5a733bee23612c35e1d0a591607631065a46c42
cezkuj <cezkuj@gmail.com>
2018-09-05 21:12:20 +0000

    Dispatcher should use other langs than us

--- analyzer/dispatcher.go
+++ analyzer/dispatcher.go
@@ -17,8 +17,13 @@ func StartDispatching(env db.Env) {
 	}
 	for {
 		for _, k := range keywords {
+			a, err := env.GetAnalyzes(k.Name, time.Time{}, time.Now(), "any")
+			if err != nil {
+				log.Error(err)
+				return
+			}
 			log.Info(fmt.Sprintf("Started analyzing: %v", k))
-			go Analyze(env, k.Name, "both", "us", "any")
+			go Analyze(env, k.Name, "both", a[0].Country, "any")
 			time.Sleep(3 * time.Minute)
 		}
 
