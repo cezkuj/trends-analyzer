302292db3e5333bd20bf789637ad4248b19bbdd3
cezkuj <cezkuj@gmail.com>
2018-09-10 10:59:43 +0000

    wiremock fixes

--- wiremock/wiremock.go
+++ wiremock/wiremock.go
@@ -1,13 +1,15 @@
 package wiremock
 
-import(
-"os/exec"
+import (
+	"os/exec"
+)
 
-func runWiremockIfNotStarted() error{
-   return exec.Run("docker run -itd -v $PWD/mappings:/home/wiremock/mappings -p 8080:8080 --name wiremock wiremock")
+func runWiremockIfNotStarted() error {
+	cmd := exec.Command("docker run -itd -v $PWD/mappings:/home/wiremock/mappings -p 8080:8080 --name wiremock wiremock")
+	return cmd.Run()
 }
 
-func stopWiremock(){
-   return exec.Run("docker stop wiremock")
-
+func stopWiremock() error {
+	cmd := exec.Command("docker stop wiremock")
+	return cmd.Run()
 }
